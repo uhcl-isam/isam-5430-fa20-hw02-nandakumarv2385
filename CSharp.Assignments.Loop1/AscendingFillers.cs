8d2fee382e2bd6d5aa1288e04167f70e6c80c7f7
Vivek Nandakumar <nandakumarv2385@uhcl.edu>
2020-09-14 15:33:50 -0500

    .

--- CSharp.Assignments.Loop1/AscendingFillers.cs
+++ CSharp.Assignments.Loop1/AscendingFillers.cs
@@ -17,6 +17,64 @@ namespace CSharp.Assignments.Loop1
         public static void Main()
         {
             // Complete your loop codes here.
+            bool negative = false;
+            int count = 0;
+            List<int> number = new List<int>();
+            List<int> extra = new List<int>();
+            while (negative == false)
+            {
+                string input = Console.ReadLine();
+                int n;
+                Int32.TryParse(input, out n);
+                if (n < 0)
+                {
+                    negative = true;
+                }
+                else
+                {
+                    if ((number.Count) == 0)
+                    {
+                        number.Insert(count, n);
+                        count = count + 1;
+                    }
+                    else
+                    {
+                        if (n == 0)
+                        {
+                            number.Insert(count, n);
+                            count = count + 1;
+                        }
+                        else if (n == number.ElementAt(count - 1))
+                        {
+                            n = n + 1;
+                            extra.Add(1);
+                            number.Insert(count, n);
+                            count = count + 1;
+                        }
+                        else if (n < number.ElementAt(count - 1))
+                        {
+                            int a;
+                            int b;
+                            a = n;
+                            a = number.ElementAt(count - 1) + 1;
+                            b = a - n;
+                            extra.Add(b);
+                            number.Insert(count, a);
+                            count = count + 1;
+                        }
+                        else
+                        {
+                            number.Insert(count, n);
+                            count = count + 1;
+                        }
+                    }
+                }
+            }
+            foreach (int a in number)
+                Console.WriteLine(a);
+            int sum = extra.AsQueryable().Sum();
+            Console.WriteLine("\nSum:");
+            Console.WriteLine(sum);
         }
     }
 }
--- CSharp.Assignments.Loop1/FibonacciNumbers.cs
+++ CSharp.Assignments.Loop1/FibonacciNumbers.cs
@@ -10,11 +10,32 @@ namespace CSharp.Assignments.Loop1
     /// </summary>
     public class FibonacciNumbers
     {
-        public static void Main()
+        public static int FibonacciNumbers(int n)
         {
-            Console.WriteLine("Enter the last number of Fibonacci Sequence of numbers.");
-            int end = int.Parse(Console.ReadLine());
+            int p = 0;
+            int q = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int temp = p;
+                p = q;
+                q = temp + q;
+            }
+            return p;
+        }
 
+        static void Main()
+        {
+            Console.WriteLine("\n\n Recursion : Find the Fibonacci numbers for a n numbers of series :");
+            Console.WriteLine("-----------------------------------------------------------------------");
+
+            Console.Write(" Input number of terms for the Fibonacci series : ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n The Fibonacci series of {0} terms is : ", n1);
+            for (int i = 0; i < n1; i++)
+            {
+                Console.Write("{0} ", FibonacciNumbers(i));
+            }
+            Console.ReadKey();
         }
     }
 }
